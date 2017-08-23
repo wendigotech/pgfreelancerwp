f9464f7a100a6ff1fdee5800165b090356496470
wendigotech <wendigotech@gmail.com>
2017-08-24 01:18:43 +0200

    add bangers font

--- functions.php
+++ functions.php
@@ -172,10 +172,10 @@ if ( ! function_exists( 'freelancer_enqueue_scripts' ) ) :
     wp_enqueue_style( 'custom', get_template_directory_uri() . '/custom.css', false, null, 'all');
 
     wp_deregister_style( 'style-3' );
-    wp_enqueue_style( 'style-3', 'http://fonts.googleapis.com/css?family=Bangers:400', false, null, 'all');
+    wp_enqueue_style( 'style-3', 'https://fonts.googleapis.com/css?family=Bangers:400', false, null, 'all');
 
     wp_deregister_style( 'style-4' );
-    wp_enqueue_style( 'style-4', 'http://fonts.googleapis.com/css?family=Akronim:400', false, null, 'all');
+    wp_enqueue_style( 'style-4', 'https://fonts.googleapis.com/css?family=Akronim:400', false, null, 'all');
 
     /* Pinegrow generated Enqueue Styles End */
 
